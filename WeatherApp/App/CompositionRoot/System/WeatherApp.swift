3e804dac25399b348cd6a4070fd43bc3ec7bce55
Jose Lopez <vashim_vogom@yahoo.com>
2025-01-17 14:20:48 -0600

    (BaseProject) Adding XcodeProject and Base App Architecture structure

---
+++ WeatherApp/App/CompositionRoot/System/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  WeatherApp
+//
+//  Created by Lord Jose Lopez on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherApp/App/Modules/Weather/Presentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Lord Jose Lopez on 17/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
